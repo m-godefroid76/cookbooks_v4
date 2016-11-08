2962c3702105c2390118235df6ffc5060b047b35
m-godefroid76 <michael.godefroid@opengraphy.com>
2016-11-08 15:43:31 +0000

    new recipes

--- wp-config/recipes/default.rb
+++ wp-config/recipes/default.rb
@@ -4,3 +4,10 @@ template '/mnt/srv/www/wordpress/current/wp-config.php' do
   group 'root'
   mode '0444'
 end
+
+template '/srv/www/wordpress/current/.htaccess' do
+  source 'htaccess.erb'
+  owner 'root'
+  group 'root'
+  mode '0444'
+end
