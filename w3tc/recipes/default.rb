a02f0114bcb5110b0dbe197e999f1854ef22eab1
m-godefroid76 <michael.godefroid@opengraphy.com>
2016-11-22 15:56:02 +0000

    chmod w3tc

--- w3tc/recipes/default.rb
+++ w3tc/recipes/default.rb
@@ -3,13 +3,6 @@
 # Recipe:: default
 # encoding: utf-8
 #
-directory '/srv/www/wordpress/current/wp-content/cache/w3tc-config' do
-  owner 'www-data'
-  group 'www-data'
-  mode '0777'
-  action :create
-end
-
 template '/srv/www/wordpress/current/wp-content/w3tc-config/master.json' do
   source 'master.json.erb'
   owner 'www-data'
@@ -22,4 +15,16 @@ template '/srv/www/wordpress/current/wp-content/w3tc-config/index.html' do
   owner 'www-data'
   group 'www-data'
   mode '0777'
+end
+
+node[:deploy].each do |application, deploy|
+  www_folder = "/srv/www/wordpress/current/wp-content/w3tc-config"
+  execute "chown -R www-data:www-data #{www_folder}" do
+  end
+end
+
+node[:deploy].each do |application, deploy|
+  www_folder = "/srv/www/wordpress/current/wp-content/w3tc-config"
+  execute "chmod -R 777 #{www_folder}" do
+  end
 end
